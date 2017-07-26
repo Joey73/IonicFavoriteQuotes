69ab83c58b7b67427baab7c514e764097e60e8ac
Joerg Schmitz <schmitz873@gmail.com>
2017-07-27 00:45:53 +0200

    Initial Commit

---
+++ src/pages/quotes/quotes.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams, AlertController } from "ionic-angular";
+
+import { Quote } from "../../data/quote.interface";
+
+@Component({
+  selector: 'page-quotes',
+  templateUrl: 'quotes.html',
+})
+export class QuotesPage implements OnInit{
+  quoteGroup: { category: string, quotes: Quote[], icon: string };
+
+  constructor(private navParams: NavParams, private alertCtrl: AlertController){}
+
+  ngOnInit(): void {
+    this.quoteGroup = this.navParams.data;
+  }
+
+  // ionViewDidLoad() {
+  //   this.quoteGroup = this.navParams.data;
+  // Add elvis operator (?) to quoteGroup in HTML to use this approach
+  // }
+
+  onAddToFavorite(selectedQuote: Quote){
+    const alert = this.alertCtrl.create({
+      title: 'Add Quote',
+      subTitle: 'Are you sure?',
+      message: 'Are you sure you want to add the quote?',
+      buttons: [
+        {
+          text: 'Yes, go ahead',
+          handler: () => {
+            console.log('Ok');
+          }
+        },
+        {
+          text: 'No, I changed my mind',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelled');
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
+}
